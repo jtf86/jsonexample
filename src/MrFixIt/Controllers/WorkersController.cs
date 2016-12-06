c6f369b86dfe480bf9ea8af77f933d5bc97112c6
John <jfranti@gmail.com>
2016-12-05 17:53:44 -0800

    end of day monday

--- src/MrFixIt/Controllers/WorkersController.cs
+++ src/MrFixIt/Controllers/WorkersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MrFixIt.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,16 +11,25 @@ namespace MrFixIt.Controllers
 {
     public class WorkersController : Controller
     {
+        private MrFixItContext db = new MrFixItContext();
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Worker worker)
+        {
+            db.Workers.Add(worker); 
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
